ac9fc884b238bd9dee55f63e7a72050e0cddc0fa
ROBILLIARD <82404725+Fysalidev@users.noreply.github.com>
2022-03-15 20:41:33 +0100

    Mise en place de la modale

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,9 +1,15 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-	modal.style.display = "block";
+    const main = document.getElementById('main')
+	
+    modal.style.display = "block";
+    main.style.display = 'none'
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
+    const main = document.getElementById('main')
+    
     modal.style.display = "none";
+    main.style.display ='block'
 }
