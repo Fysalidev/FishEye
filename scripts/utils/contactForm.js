00528bd5f5b94fc45cccb60cceb3013c31ad977c
ROBILLIARD <82404725+Fysalidev@users.noreply.github.com>
2022-03-15 23:26:53 +0100

    Formulaire en cours

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -1,15 +1,9 @@
 function displayModal() {
     const modal = document.getElementById("contact_modal");
-    const main = document.getElementById('main')
-	
     modal.style.display = "block";
-    main.style.display = 'none'
 }
 
 function closeModal() {
     const modal = document.getElementById("contact_modal");
-    const main = document.getElementById('main')
-    
     modal.style.display = "none";
-    main.style.display ='block'
 }
