5fee225b97450f8e7651ded6a536d09b350d04c1
ROBILLIARD <82404725+Fysalidev@users.noreply.github.com>
2022-03-25 18:21:25 +0100

    vendredi soir....

--- scripts/utils/contactForm.js
+++ scripts/utils/contactForm.js
@@ -3,17 +3,10 @@ function displayModal() {
     const main = document.getElementById('main')
     const modal = document.getElementById("contact_modal")
     const modalBtnClose = document.getElementById('modal-btn-close')
-    console.log(body)
 
     modal.style.display = "block";
     body.setAttribute('aria-hidden', 'true')
     body.classList.add('no-scroll')
-   
-       $mainWrapper.attr("aria-hidden", "true");
-       $modal.attr("aria-hidden", "false");
-       $body.addClass("no-scroll");
-       $modal.css("display", "flex");
-       $modalCloseBtn.focus();
 }
 
 function closeModal() {
